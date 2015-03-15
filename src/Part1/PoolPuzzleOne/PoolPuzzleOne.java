c0bde91fb7f9ef516d378fa93654d8b7f82e7a76
mastytchmac <mastytch@gmail.com>
2015-03-15 22:20:15 +0200

    not working yet

---
+++ src/Part1/PoolPuzzleOne/PoolPuzzleOne.java
@@ -0,0 +1,21 @@
+/*
+package Part1.PoolPuzzleOne;
+
+public class PoolPuzzleOne {
+    public static void main(String[] args) {
+        int x = 0;
+        while () {
+            if () {
+
+            }
+            if () {
+
+            }
+            if () {
+
+            }
+            System.out.println("");
+        }
+    }
+}
+*/
---
+++ src/Part2/GuessGame/GameLauncher.java
@@ -0,0 +1,8 @@
+package Part2.GuessGame;
+
+public class GameLauncher {
+    public static void main(String[] args) {
+        GuessGame game = new GuessGame();
+        game.startGame();
+    }
+}
