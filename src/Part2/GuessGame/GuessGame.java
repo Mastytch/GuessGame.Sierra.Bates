1d81e101d7dfc7a4947c2857fd5abcf85fc22cf1
mastytchmac <mastytch@gmail.com>
2015-03-15 22:48:54 +0200

    работает!

--- src/Part2/GuessGame/GuessGame.java
+++ src/Part2/GuessGame/GuessGame.java
@@ -6,6 +6,10 @@ public class GuessGame {
     Player p3;
 
     public void startGame(){
+        p1 = new Player();
+        p2 = new Player();
+        p3 = new Player();
+
         int guessp1 = 0;
         int guessp2 = 0;
         int guessp3 = 0;
@@ -17,7 +21,7 @@ public class GuessGame {
         int targetNumber = (int)(Math.random() * 10);
         System.out.println("Я загадываю число от 0 до 9...");
 
-        while (true){
+        while (true) {
             System.out.println("Число, которое нужно угадать, - " + targetNumber);
 
             p1.guess();
